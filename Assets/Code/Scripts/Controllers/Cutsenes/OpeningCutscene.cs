bf68987277e1e178b97a5fa9f478bb647e17bcce
shishcabob <shishcabobgames@gmail.com>
2023-02-20 21:46:18 -0800

    Cutscene setup

---
+++ Assets/Code/Scripts/Controllers/Cutsenes/OpeningCutscene.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Coots.Code.Controllers.Cutsenes
+{
+    public class OpeningCutscene : MonoBehaviour
+    {
+        #region Variables
+
+        #endregion
+
+        #region Unity Methods
+        private void Update()
+        {
+            if (Input.GetKey(KeyCode.LeftShift))
+            {
+                Time.timeScale = 5;
+            }
+            else
+            {
+                Time.timeScale = 1;
+            }
+            if (Input.GetKeyDown(KeyCode.Space)){
+                GoToGameScene();
+            }
+        }
+        #endregion
+
+        #region Methods
+        public void GoToGameScene()
+        {
+            Time.timeScale = 1;
+            SceneManager.LoadScene("GameWorld");
+        }
+        #endregion
+    }
+}
